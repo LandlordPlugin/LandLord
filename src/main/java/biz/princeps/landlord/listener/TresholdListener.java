6090827ebf47a1b9dbc66ae54f92bf8cd4cafb07
SpatiumPrinceps <spatium@princeps.biz>
2017-12-03 23:24:49 +0100

    HOTFIX: Treshold not working properly fixed

--- src/main/java/biz/princeps/landlord/listener/TresholdListener.java
+++ src/main/java/biz/princeps/landlord/listener/TresholdListener.java
@@ -2,6 +2,7 @@ package biz.princeps.landlord.listener;
 
 import biz.princeps.landlord.util.OwnedLand;
 import biz.princeps.lib.PrincepsLib;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
@@ -32,7 +33,7 @@ public class TresholdListener extends BasicListener {
         Player p = e.getPlayer();
         OwnedLand land = plugin.getLand(e.getBlock().getLocation());
 
-        handleLand(p, land, e);
+        handleLand(p, e.getBlock().getLocation(), land, e);
     }
 
     @EventHandler
@@ -41,7 +42,7 @@ public class TresholdListener extends BasicListener {
         Player p = e.getPlayer();
         OwnedLand land = plugin.getLand(e.getBlock().getLocation());
 
-        handleLand(p, land, e);
+        handleLand(p, e.getBlockPlaced().getLocation(), land, e);
     }
 
     @EventHandler
@@ -50,23 +51,28 @@ public class TresholdListener extends BasicListener {
         Player p = e.getPlayer();
         OwnedLand land = plugin.getLand(e.getBlockClicked().getLocation());
 
-        handleLand(p, land, e);
+        handleLand(p, e.getBlockClicked().getLocation(), land, e);
     }
 
-    public void handleLand(Player p, OwnedLand land, Cancellable e) {
+    public void handleLand(Player p, Location loc, OwnedLand land, Cancellable e) {
         // is free land
-        if (land == null) {
-            int landcount = plugin.getWgHandler().getRegionCountOfPlayer(p.getUniqueId());
+        if (p.isOp() || p.hasPermission("landlord.admin.bypass"))
+            return;
 
-            if (landcount < treshold) {
+        if (!plugin.getConfig().getStringList("disabled-worlds").contains(loc.getWorld().getName())) {
+            if (land == null) {
+                int landcount = plugin.getWgHandler().getRegionCountOfPlayer(p.getUniqueId());
 
-                if (display == LandAlerter.LandMessageDisplay.ActionBar) {
-                    PrincepsLib.crossVersion().sendActionBar(p, plugin.getLangManager().getRawString("Alerts.tresholdNotReached").replace("%x%", treshold + ""));
-                } else if (display == LandAlerter.LandMessageDisplay.Chat) {
-                    p.sendMessage(plugin.getLangManager().getString("Alerts.tresholdNotReached").replace("%x%", treshold + ""));
+                if (landcount < treshold) {
+
+                    if (display == LandAlerter.LandMessageDisplay.ActionBar) {
+                        PrincepsLib.crossVersion().sendActionBar(p, plugin.getLangManager().getRawString("Alerts.tresholdNotReached").replace("%x%", treshold + ""));
+                    } else if (display == LandAlerter.LandMessageDisplay.Chat) {
+                        p.sendMessage(plugin.getLangManager().getString("Alerts.tresholdNotReached").replace("%x%", treshold + ""));
+                    }
+
+                    e.setCancelled(true);
                 }
-
-                e.setCancelled(true);
             }
         }
     }
