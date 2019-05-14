a68249c5b70798dce13f3220d54665a6ec373f65
Alexander Daichendt <alexander.daichendt@tum.de>
2019-05-14 17:23:58 +0200

    added some more documentation

--- LandLord-core/src/main/java/biz/princeps/landlord/api/IDelimitationManager.java
+++ LandLord-core/src/main/java/biz/princeps/landlord/api/IDelimitationManager.java
@@ -5,5 +5,13 @@ import org.bukkit.entity.Player;
 
 public interface IDelimitationManager {
 
+    /**
+     * Delimits a chunk for a player if enabled.
+     * Takes a string list (CommandSettings.Claim.delimitation) as a pattern.
+     * Also the delimitation may be phantomblocks only.
+     *
+     * @param player the player to delimit the chunk for
+     * @param chunk  the chunk to delimit
+     */
     void delimit(Player player, Chunk chunk);
 }
