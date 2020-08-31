e059be953a0b2ff62deccf2b1499fb82a912aa8c
Alexander Daichendt <alexander.daichendt@tum.de>
2020-08-31 15:38:14 +0200

    fix: disabled world is respected by ll update

--- LandLord-core/src/main/java/biz/princeps/landlord/commands/admin/Update.java
+++ LandLord-core/src/main/java/biz/princeps/landlord/commands/admin/Update.java
@@ -70,6 +70,7 @@ public class Update extends LandlordCommand {
 
             Collection<IOwnedLand> regions = new HashSet<>();
             for (World world : Bukkit.getWorlds()) {
+                if(isDisabledWorld(world)) continue;
                 regions.addAll(plugin.getWGManager().getRegions(world));
             }
 
