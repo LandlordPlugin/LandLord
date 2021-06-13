1bf2e694c67413f6e39908d9efce60d78a741970
Aurélien <43724816+Aurelien30000@users.noreply.github.com>
2021-06-13 21:59:28 +0200

    Update LandLord.java

--- LandLord-latest/src/main/java/biz/princeps/landlord/LandLord.java
+++ LandLord-latest/src/main/java/biz/princeps/landlord/LandLord.java
@@ -92,7 +92,7 @@ public class LandLord extends ALandLord {
         // Dependency stuff
         final String version = Bukkit.getVersion();
         if (!version.contains("1.13.2") && !version.contains("1.14") && !version.contains("1.15") && !version.contains("1.16") && !version.contains("1.17")) {
-            haltPlugin("Invalid Spigot version detected! LandLord latest requires 1.13.2/1.14.x/1.15.x/1.16.x, use Legacy version for 1.12.2!");
+            haltPlugin("Invalid Spigot version detected! LandLord latest requires 1.13.2/1.14.x/1.15.x/1.16.x/1.17.x, use Legacy version for 1.12.2!");
             return false;
         }
 
