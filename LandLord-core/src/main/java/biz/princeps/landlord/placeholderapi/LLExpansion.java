c3d159098ce325da064f56f0ac07efca2f4898c2
Aurélien <43724816+Aurelien30000@users.noreply.github.com>
2021-06-24 19:04:51 +0200

    Update LLExpansion.java

--- LandLord-core/src/main/java/biz/princeps/landlord/placeholderapi/LLExpansion.java
+++ LandLord-core/src/main/java/biz/princeps/landlord/placeholderapi/LLExpansion.java
@@ -55,19 +55,13 @@ public class LLExpansion extends PlaceholderExpansion {
                 return String.valueOf(iPlayer.getClaims());
 
             case "remainingClaims":
-                final int freeClaims = pl.getConfig().getInt("Claims.free");
-
-                if (landCount <= freeClaims) {
-                    final IPlayer iPlayer2 = pl.getPlayerManager().get(player.getUniqueId());
-                    if (iPlayer2 == null) {
-                        pl.getLogger().warning("A placeholder is trying to load %landlord_remainingClaims% before async loading of the " +
-                                "player has finished! Use FinishedLoadingPlayerEvent!");
-                        return "NaN";
-                    }
-                    return String.valueOf(iPlayer2.getClaims() - landCount);
-                } else {
-                    return "0";
+                final IPlayer iPlayer2 = pl.getPlayerManager().get(player.getUniqueId());
+                if (iPlayer2 == null) {
+                    pl.getLogger().warning("A placeholder is trying to load %landlord_remainingClaims% before async loading of the " +
+                            "player has finished! Use FinishedLoadingPlayerEvent!");
+                    return "NaN";
                 }
+                return String.valueOf(iPlayer2.getClaims() - landCount);
 
             case "currentLandOwner":
                 if (region != null) {
