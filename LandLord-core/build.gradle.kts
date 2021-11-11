bfc0d1c33b29a2db3e556135505561c69ee6aaf6
Aurélien <43724816+Aurelien30000@users.noreply.github.com>
2021-11-11 12:29:38 +0100

    Bump eldo-util

--- LandLord-core/build.gradle.kts
+++ LandLord-core/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     api(project(":LandLord-api"))
     implementation("com.zaxxer:HikariCP:4.0.3")
     implementation("io.github.bananapuncher714:nbteditor:7.16.1")
-    implementation("de.eldoria:eldo-util:1.9.2-DEV")
+    implementation("de.eldoria:eldo-util:1.12.0-DEV")
     compileOnly("org.spigotmc:spigot-api:1.12.2-R0.1-SNAPSHOT")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
     compileOnly("me.clip:placeholderapi:2.10.9")
