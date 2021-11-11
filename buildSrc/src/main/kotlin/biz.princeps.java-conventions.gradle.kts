8f16de0134ff7915ccca28c779ebe7614474dcf8
Aurelien30000 <aurelien.domino@gmail.com>
2021-11-11 19:50:45 +0100

    Oops

--- buildSrc/src/main/kotlin/biz.princeps.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/biz.princeps.java-conventions.gradle.kts
@@ -27,7 +27,7 @@ allprojects {
     java {
         withSourcesJar()
         withJavadocJar()
-        sourceCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
