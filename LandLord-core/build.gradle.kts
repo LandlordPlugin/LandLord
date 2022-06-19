1e29c65caea384d806f7b22008e4ee8f37456066
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-06-19 20:12:29 +0200

    Add publishing for api and core

--- LandLord-core/build.gradle.kts
+++ LandLord-core/build.gradle.kts
@@ -17,3 +17,30 @@ dependencies {
 }
 
 description = "LandLord-core"
+
+publishData {
+    useEldoNexusRepos()
+    publishComponent("java")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            publishData.configurePublication(this)
+        }
+    }
+
+    repositories {
+        maven {
+            name = "EldoNexus"
+            url = uri(publishData.getRepository())
+
+            authentication {
+                credentials(PasswordCredentials::class) {
+                    username = System.getenv("NEXUS_USERNAME")
+                    password = System.getenv("NEXUS_PASSWORD")
+                }
+            }
+        }
+    }
+}
