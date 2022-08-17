a9f91912d561e227aa52a092fa6c363c4286777f
Aurélien <43724816+Aurelien30000@users.noreply.github.com>
2022-08-17 11:42:48 +0200

    Address #57, use mob heads instead of spawn eggs

--- LandLord-latest/src/main/java/biz/princeps/landlord/manager/MobsManager.java
+++ LandLord-latest/src/main/java/biz/princeps/landlord/manager/MobsManager.java
@@ -3,6 +3,7 @@ package biz.princeps.landlord.manager;
 import biz.princeps.landlord.api.ILandLord;
 import biz.princeps.landlord.api.IMob;
 import biz.princeps.landlord.api.IMobManager;
+import biz.princeps.landlord.util.Skulls;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
@@ -46,101 +47,113 @@ public class MobsManager implements IMobManager {
             register1_17Entities();
         }
 
+        if (currentDataVersion >= 3105) {
+            register1_19Entities();
+        }
+
         MOBS.sort(Comparator.comparing(iMob -> iMob.getType().name()));
     }
 
     private void registerDefaultEntities() {
         // Default entities (>= 1.13 currently)
-        Mob ELDER_GUARDIAN = new Mob(EntityType.ELDER_GUARDIAN, Material.ELDER_GUARDIAN_SPAWN_EGG);
-        Mob WITHER_SKELETON = new Mob(EntityType.WITHER_SKELETON, Material.WITHER_SKELETON_SPAWN_EGG);
-        Mob STRAY = new Mob(EntityType.STRAY, Material.STRAY_SPAWN_EGG);
-        Mob HUSK = new Mob(EntityType.HUSK, Material.HUSK_SPAWN_EGG);
-        Mob ZOMBIE_VILLAGER = new Mob(EntityType.ZOMBIE_VILLAGER, Material.ZOMBIE_VILLAGER_SPAWN_EGG);
-        Mob SKELETON_HORSE = new Mob(EntityType.SKELETON_HORSE, Material.SKELETON_HORSE_SPAWN_EGG);
-        Mob ZOMBIE_HORSE = new Mob(EntityType.ZOMBIE_HORSE, Material.ZOMBIE_HORSE_SPAWN_EGG);
-        Mob DONKEY = new Mob(EntityType.DONKEY, Material.DONKEY_SPAWN_EGG);
-        Mob MULE = new Mob(EntityType.MULE, Material.MULE_SPAWN_EGG);
-        Mob EVOKER = new Mob(EntityType.EVOKER, Material.EVOKER_SPAWN_EGG);
-        Mob VEX = new Mob(EntityType.VEX, Material.VEX_SPAWN_EGG);
-        Mob VINDICATOR = new Mob(EntityType.VINDICATOR, Material.VINDICATOR_SPAWN_EGG);
-        Mob CREEPER = new Mob(EntityType.CREEPER, Material.CREEPER_SPAWN_EGG);
-        Mob SKELETON = new Mob(EntityType.SKELETON, Material.SKELETON_SPAWN_EGG);
-        Mob SPIDER = new Mob(EntityType.SPIDER, Material.SPIDER_SPAWN_EGG);
-        Mob ZOMBIE = new Mob(EntityType.ZOMBIE, Material.ZOMBIE_SPAWN_EGG);
-        Mob SLIME = new Mob(EntityType.SLIME, Material.SLIME_SPAWN_EGG);
-        Mob GHAST = new Mob(EntityType.GHAST, Material.GHAST_SPAWN_EGG);
-        Mob ENDERMAN = new Mob(EntityType.ENDERMAN, Material.ENDERMAN_SPAWN_EGG);
-        Mob CAVE_SPIDER = new Mob(EntityType.CAVE_SPIDER, Material.CAVE_SPIDER_SPAWN_EGG);
-        Mob SILVERFISH = new Mob(EntityType.SILVERFISH, Material.SILVERFISH_SPAWN_EGG);
-        Mob BLAZE = new Mob(EntityType.BLAZE, Material.BLAZE_SPAWN_EGG);
-        Mob MAGMA_CUBE = new Mob(EntityType.MAGMA_CUBE, Material.MAGMA_CUBE_SPAWN_EGG);
-        Mob BAT = new Mob(EntityType.BAT, Material.BAT_SPAWN_EGG);
-        Mob WITCH = new Mob(EntityType.WITCH, Material.WITCH_SPAWN_EGG);
-        Mob GUARDIAN = new Mob(EntityType.GUARDIAN, Material.GUARDIAN_SPAWN_EGG);
-        Mob PIG = new Mob(EntityType.PIG, Material.PIG_SPAWN_EGG);
-        Mob SHEEP = new Mob(EntityType.SHEEP, Material.SHEEP_SPAWN_EGG);
-        Mob COW = new Mob(EntityType.COW, Material.COW_SPAWN_EGG);
-        Mob CHICKEN = new Mob(EntityType.CHICKEN, Material.CHICKEN_SPAWN_EGG);
-        Mob SQUID = new Mob(EntityType.SQUID, Material.SQUID_SPAWN_EGG);
-        Mob WOLF = new Mob(EntityType.WOLF, Material.WOLF_SPAWN_EGG);
-        Mob OCELOT = new Mob(EntityType.OCELOT, Material.OCELOT_SPAWN_EGG);
-        Mob HORSE = new Mob(EntityType.HORSE, Material.HORSE_SPAWN_EGG);
-        Mob RABBIT = new Mob(EntityType.RABBIT, Material.RABBIT_SPAWN_EGG);
-        Mob POLAR_BEAR = new Mob(EntityType.POLAR_BEAR, Material.POLAR_BEAR_SPAWN_EGG);
-        Mob LLAMA = new Mob(EntityType.LLAMA, Material.LLAMA_SPAWN_EGG);
-        Mob PARROT = new Mob(EntityType.PARROT, Material.PARROT_SPAWN_EGG);
-        Mob VILLAGER = new Mob(EntityType.VILLAGER, Material.VILLAGER_SPAWN_EGG);
-        Mob TURTLE = new Mob(EntityType.TURTLE, Material.TURTLE_SPAWN_EGG);
-        Mob PHANTOM = new Mob(EntityType.PHANTOM, Material.PHANTOM_SPAWN_EGG);
-        Mob COD = new Mob(EntityType.COD, Material.COD_SPAWN_EGG);
-        Mob SALMON = new Mob(EntityType.SALMON, Material.SALMON_SPAWN_EGG);
-        Mob PUFFERFISH = new Mob(EntityType.PUFFERFISH, Material.PUFFERFISH_SPAWN_EGG);
-        Mob TROPICAL_FISH = new Mob(EntityType.TROPICAL_FISH, Material.TROPICAL_FISH_SPAWN_EGG);
-        Mob DROWNED = new Mob(EntityType.DROWNED, Material.DROWNED_SPAWN_EGG);
-        Mob DOLPHIN = new Mob(EntityType.DOLPHIN, Material.DOLPHIN_SPAWN_EGG);
-        Mob MUSHROOM_COW = new Mob(EntityType.MUSHROOM_COW, Material.MOOSHROOM_SPAWN_EGG);
+        Mob ELDER_GUARDIAN = new Mob(EntityType.ELDER_GUARDIAN, Skulls.ELDER_GUARDIAN.getSkull(plugin));
+        Mob WITHER_SKELETON = new Mob(EntityType.WITHER_SKELETON, new ItemStack(Material.WITHER_SKELETON_SKULL));
+        Mob STRAY = new Mob(EntityType.STRAY, Skulls.STRAY.getSkull(plugin));
+        Mob HUSK = new Mob(EntityType.HUSK, Skulls.HUSK.getSkull(plugin));
+        Mob ZOMBIE_VILLAGER = new Mob(EntityType.ZOMBIE_VILLAGER, Skulls.ZOMBIE_VILLAGER.getSkull(plugin));
+        Mob SKELETON_HORSE = new Mob(EntityType.SKELETON_HORSE, Skulls.SKELETON_HORSE.getSkull(plugin));
+        Mob ZOMBIE_HORSE = new Mob(EntityType.ZOMBIE_HORSE, Skulls.ZOMBIE_HORSE.getSkull(plugin));
+        Mob DONKEY = new Mob(EntityType.DONKEY, Skulls.DONKEY.getSkull(plugin));
+        Mob MULE = new Mob(EntityType.MULE, Skulls.MULE.getSkull(plugin));
+        Mob EVOKER = new Mob(EntityType.EVOKER, Skulls.EVOKER.getSkull(plugin));
+        Mob VEX = new Mob(EntityType.VEX, Skulls.VEX.getSkull(plugin));
+        Mob VINDICATOR = new Mob(EntityType.VINDICATOR, Skulls.VINDICATOR.getSkull(plugin));
+        Mob CREEPER = new Mob(EntityType.CREEPER, new ItemStack(Material.CREEPER_HEAD));
+        Mob SKELETON = new Mob(EntityType.SKELETON, new ItemStack(Material.SKELETON_SKULL));
+        Mob SPIDER = new Mob(EntityType.SPIDER, Skulls.SPIDER.getSkull(plugin));
+        Mob ZOMBIE = new Mob(EntityType.ZOMBIE, new ItemStack(Material.ZOMBIE_HEAD));
+        Mob SLIME = new Mob(EntityType.SLIME, Skulls.SLIME.getSkull(plugin));
+        Mob GHAST = new Mob(EntityType.GHAST, Skulls.GHAST.getSkull(plugin));
+        Mob ENDERMAN = new Mob(EntityType.ENDERMAN, Skulls.ENDERMAN.getSkull(plugin));
+        Mob CAVE_SPIDER = new Mob(EntityType.CAVE_SPIDER, Skulls.CAVE_SPIDER.getSkull(plugin));
+        Mob SILVERFISH = new Mob(EntityType.SILVERFISH, Skulls.SILVERFISH.getSkull(plugin));
+        Mob BLAZE = new Mob(EntityType.BLAZE, Skulls.BLAZE.getSkull(plugin));
+        Mob MAGMA_CUBE = new Mob(EntityType.MAGMA_CUBE, Skulls.MAGMA_CUBE.getSkull(plugin));
+        Mob BAT = new Mob(EntityType.BAT, Skulls.BAT.getSkull(plugin));
+        Mob WITCH = new Mob(EntityType.WITCH, Skulls.WITCH.getSkull(plugin));
+        Mob GUARDIAN = new Mob(EntityType.GUARDIAN, Skulls.GUARDIAN.getSkull(plugin));
+        Mob PIG = new Mob(EntityType.PIG, Skulls.PIG.getSkull(plugin));
+        Mob SHEEP = new Mob(EntityType.SHEEP, Skulls.WHITE_SHEEP.getSkull(plugin));
+        Mob COW = new Mob(EntityType.COW, Skulls.COW.getSkull(plugin));
+        Mob CHICKEN = new Mob(EntityType.CHICKEN, Skulls.CHICKEN.getSkull(plugin));
+        Mob SQUID = new Mob(EntityType.SQUID, Skulls.SQUID.getSkull(plugin));
+        Mob WOLF = new Mob(EntityType.WOLF, Skulls.WOLF.getSkull(plugin));
+        Mob OCELOT = new Mob(EntityType.OCELOT, Skulls.OCELOT.getSkull(plugin));
+        Mob HORSE = new Mob(EntityType.HORSE, Skulls.WHITE_HORSE.getSkull(plugin));
+        Mob RABBIT = new Mob(EntityType.RABBIT, Skulls.BROWN_RABBIT.getSkull(plugin));
+        Mob POLAR_BEAR = new Mob(EntityType.POLAR_BEAR, Skulls.POLAR_BEAR.getSkull(plugin));
+        Mob LLAMA = new Mob(EntityType.LLAMA, Skulls.CREAMY_LLAMA.getSkull(plugin));
+        Mob PARROT = new Mob(EntityType.PARROT, Skulls.RED_PARROT.getSkull(plugin));
+        Mob VILLAGER = new Mob(EntityType.VILLAGER, Skulls.UNEMPLOYED_VILLAGER.getSkull(plugin));
+        Mob TURTLE = new Mob(EntityType.TURTLE, Skulls.TURTLE.getSkull(plugin));
+        Mob PHANTOM = new Mob(EntityType.PHANTOM, Skulls.PHANTOM.getSkull(plugin));
+        Mob COD = new Mob(EntityType.COD, Skulls.COD.getSkull(plugin));
+        Mob SALMON = new Mob(EntityType.SALMON, Skulls.SALMON.getSkull(plugin));
+        Mob PUFFERFISH = new Mob(EntityType.PUFFERFISH, Skulls.PUFFERFISH.getSkull(plugin));
+        Mob TROPICAL_FISH = new Mob(EntityType.TROPICAL_FISH, Skulls.TROPICAL_FISH.getSkull(plugin));
+        Mob DROWNED = new Mob(EntityType.DROWNED, Skulls.DROWNED.getSkull(plugin));
+        Mob DOLPHIN = new Mob(EntityType.DOLPHIN, Skulls.DOLPHIN.getSkull(plugin));
+        Mob MUSHROOM_COW = new Mob(EntityType.MUSHROOM_COW, Skulls.RED_MOOSHROOM.getSkull(plugin));
 
         // PIG_ZOMBIE still exists in 1.15.1-
         if (currentDataVersion <= 2227) {
-            Mob PIG_ZOMBIE = new Mob(EntityType.valueOf("PIG_ZOMBIE"), Material.valueOf("ZOMBIE_PIGMAN_SPAWN_EGG"));
+            Mob PIG_ZOMBIE = new Mob(EntityType.valueOf("PIG_ZOMBIE"), Skulls.ZOMBIFIED_PIGLIN.getSkull(plugin));
         }
     }
 
     private void register1_14Entities() {
         // 1.14's entities
-        Mob CAT = new Mob(EntityType.CAT, Material.CAT_SPAWN_EGG);
-        Mob TRADER_LLAMA = new Mob(EntityType.TRADER_LLAMA, Material.TRADER_LLAMA_SPAWN_EGG);
-        Mob WANDERING_TRADER = new Mob(EntityType.WANDERING_TRADER, Material.WANDERING_TRADER_SPAWN_EGG);
-        Mob PANDA = new Mob(EntityType.PANDA, Material.PANDA_SPAWN_EGG);
-        Mob PILLAGER = new Mob(EntityType.PILLAGER, Material.PILLAGER_SPAWN_EGG);
-        Mob RAVAGER = new Mob(EntityType.RAVAGER, Material.RAVAGER_SPAWN_EGG);
-        Mob FOX = new Mob(EntityType.FOX, Material.FOX_SPAWN_EGG);
+        Mob CAT = new Mob(EntityType.CAT, Skulls.TABBY_CAT.getSkull(plugin));
+        Mob TRADER_LLAMA = new Mob(EntityType.TRADER_LLAMA, Skulls.CREAMY_TRADER_LLAMA.getSkull(plugin));
+        Mob WANDERING_TRADER = new Mob(EntityType.WANDERING_TRADER, Skulls.WANDERING_TRADER.getSkull(plugin));
+        Mob PANDA = new Mob(EntityType.PANDA, Skulls.PANDA.getSkull(plugin));
+        Mob PILLAGER = new Mob(EntityType.PILLAGER, Skulls.PILLAGER.getSkull(plugin));
+        Mob RAVAGER = new Mob(EntityType.RAVAGER, Skulls.RAVAGER.getSkull(plugin));
+        Mob FOX = new Mob(EntityType.FOX, Skulls.FOX.getSkull(plugin));
     }
 
     private void register1_15Entities() {
         // 1.15's entities
-        Mob BEE = new Mob(EntityType.BEE, Material.BEE_SPAWN_EGG);
+        Mob BEE = new Mob(EntityType.BEE, Skulls.BEE.getSkull(plugin));
     }
 
     private void register1_16Entities() {
         // 1.16's entities
-        Mob HOGLIN = new Mob(EntityType.HOGLIN, Material.HOGLIN_SPAWN_EGG);
-        Mob PIGLIN = new Mob(EntityType.PIGLIN, Material.PIGLIN_SPAWN_EGG);
-        Mob STRIDER = new Mob(EntityType.STRIDER, Material.STRIDER_SPAWN_EGG);
-        Mob ZOGLIN = new Mob(EntityType.ZOGLIN, Material.ZOGLIN_SPAWN_EGG);
-        Mob ZOMBIFIED_PIGLIN = new Mob(EntityType.ZOMBIFIED_PIGLIN, Material.ZOMBIFIED_PIGLIN_SPAWN_EGG);
+        Mob HOGLIN = new Mob(EntityType.HOGLIN, Skulls.HOGLIN.getSkull(plugin));
+        Mob PIGLIN = new Mob(EntityType.PIGLIN, Skulls.PIGLIN.getSkull(plugin));
+        Mob STRIDER = new Mob(EntityType.STRIDER, Skulls.STRIDER.getSkull(plugin));
+        Mob ZOGLIN = new Mob(EntityType.ZOGLIN, Skulls.ZOGLIN.getSkull(plugin));
+        Mob ZOMBIFIED_PIGLIN = new Mob(EntityType.ZOMBIFIED_PIGLIN, Skulls.ZOMBIFIED_PIGLIN.getSkull(plugin));
     }
 
     private void register1_16_2Entities() {
         // 1.16.2's entities
-        Mob PIGLIN_BRUTE = new Mob(EntityType.PIGLIN_BRUTE, Material.PIGLIN_BRUTE_SPAWN_EGG);
+        Mob PIGLIN_BRUTE = new Mob(EntityType.PIGLIN_BRUTE, Skulls.PIGLIN_BRUTE.getSkull(plugin));
     }
 
     private void register1_17Entities() {
         // 1.17's entities
-        Mob AXOLOTL = new Mob(EntityType.AXOLOTL, Material.AXOLOTL_SPAWN_EGG);
-        Mob GLOW_SQUID = new Mob(EntityType.GLOW_SQUID, Material.GLOW_SQUID_SPAWN_EGG);
-        Mob GOAT = new Mob(EntityType.GOAT, Material.GOAT_SPAWN_EGG);
+        Mob AXOLOTL = new Mob(EntityType.AXOLOTL, Skulls.BLUE_AXOLOTL.getSkull(plugin));
+        Mob GLOW_SQUID = new Mob(EntityType.GLOW_SQUID, Skulls.GLOW_SQUID.getSkull(plugin));
+        Mob GOAT = new Mob(EntityType.GOAT, Skulls.GOAT.getSkull(plugin));
+    }
+
+    private void register1_19Entities() {
+        // 1.19's entities
+        Mob ALLAY = new Mob(EntityType.ALLAY, Skulls.ALLAY.getSkull(plugin));
+        Mob FROG = new Mob(EntityType.FROG, Skulls.TEMPERATE_FROG.getSkull(plugin));
+        Mob TADPOLE = new Mob(EntityType.TADPOLE, Skulls.TADPOLE.getSkull(plugin));
+        Mob WARDEN = new Mob(EntityType.WARDEN, Skulls.WARDEN.getSkull(plugin));
     }
 
     @Override
@@ -171,12 +184,12 @@ public class MobsManager implements IMobManager {
     public static class Mob implements IMob {
 
         private final EntityType type;
-        private final Material egg;
+        private final ItemStack head;
         private final String permission;
 
-        Mob(EntityType type, Material egg) {
+        Mob(EntityType type, ItemStack head) {
             this.type = type;
-            this.egg = egg;
+            this.head = head;
             this.permission = "landlord.player.manage.mob-spawning." + type.name();
 
             MOBS.add(this);
@@ -188,8 +201,8 @@ public class MobsManager implements IMobManager {
         }
 
         @Override
-        public ItemStack getEgg() {
-            return new ItemStack(egg);
+        public ItemStack getHead() {
+            return head.clone();
         }
 
         @Override
--- LandLord-legacy/src/main/java/biz/princeps/landlord/manager/MobsManager.java
+++ LandLord-legacy/src/main/java/biz/princeps/landlord/manager/MobsManager.java
@@ -3,6 +3,7 @@ package biz.princeps.landlord.manager;
 import biz.princeps.landlord.api.ILandLord;
 import biz.princeps.landlord.api.IMob;
 import biz.princeps.landlord.api.IMobManager;
+import biz.princeps.landlord.util.Skulls;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
@@ -14,52 +15,54 @@ import java.util.List;
 
 public class MobsManager implements IMobManager {
 
-    static final List<IMob> MOBS = new ArrayList<>();
-    public static final Mob ELDER_GUARDIAN = new Mob(EntityType.ELDER_GUARDIAN, 4);
-    public static final Mob WITHER_SKELETON = new Mob(EntityType.WITHER_SKELETON, 5);
-    public static final Mob STRAY = new Mob(EntityType.STRAY, 6);
-    public static final Mob HUSK = new Mob(EntityType.HUSK, 23);
-    public static final Mob ZOMBIE_VILLAGER = new Mob(EntityType.ZOMBIE_VILLAGER, 27);
-    public static final Mob SKELETON_HORSE = new Mob(EntityType.SKELETON_HORSE, 28);
-    public static final Mob ZOMBIE_HORSE = new Mob(EntityType.ZOMBIE_HORSE, 29);
-    public static final Mob DONKEY = new Mob(EntityType.DONKEY, 31);
-    public static final Mob MULE = new Mob(EntityType.MULE, 32);
-    public static final Mob EVOKER = new Mob(EntityType.EVOKER, 34);
-    public static final Mob VEX = new Mob(EntityType.VEX, 35);
-    public static final Mob VINDICATOR = new Mob(EntityType.VINDICATOR, 36);
-    public static final Mob CREEPER = new Mob(EntityType.CREEPER, 50);
-    public static final Mob SKELETON = new Mob(EntityType.SKELETON, 51);
-    public static final Mob SPIDER = new Mob(EntityType.SPIDER, 52);
-    public static final Mob ZOMBIE = new Mob(EntityType.ZOMBIE, 54);
-    public static final Mob SLIME = new Mob(EntityType.SLIME, 55);
-    public static final Mob GHAST = new Mob(EntityType.GHAST, 56);
-    public static final Mob PIG_ZOMBIE = new Mob(EntityType.PIG_ZOMBIE, 57);
-    public static final Mob ENDERMAN = new Mob(EntityType.ENDERMAN, 58);
-    public static final Mob CAVE_SPIDER = new Mob(EntityType.CAVE_SPIDER, 59);
-    public static final Mob SILVERFISH = new Mob(EntityType.SILVERFISH, 60);
-    public static final Mob BLAZE = new Mob(EntityType.BLAZE, 61);
-    public static final Mob MAGMA_CUBE = new Mob(EntityType.MAGMA_CUBE, 62);
-    public static final Mob BAT = new Mob(EntityType.BAT, 65);
-    public static final Mob WITCH = new Mob(EntityType.WITCH, 66);
-    public static final Mob GUARDIAN = new Mob(EntityType.GUARDIAN, 68);
-    public static final Mob PIG = new Mob(EntityType.PIG, 90);
-    public static final Mob SHEEP = new Mob(EntityType.SHEEP, 91);
-    public static final Mob COW = new Mob(EntityType.COW, 92);
-    public static final Mob CHICKEN = new Mob(EntityType.CHICKEN, 93);
-    public static final Mob SQUID = new Mob(EntityType.SQUID, 94);
-    public static final Mob WOLF = new Mob(EntityType.WOLF, 95);
-    public static final Mob OCELOT = new Mob(EntityType.OCELOT, 98);
-    public static final Mob HORSE = new Mob(EntityType.HORSE, 100);
-    public static final Mob RABBIT = new Mob(EntityType.RABBIT, 101);
-    public static final Mob POLAR_BEAR = new Mob(EntityType.POLAR_BEAR, 102);
-    public static final Mob LLAMA = new Mob(EntityType.LLAMA, 103);
-    public static final Mob PARROT = new Mob(EntityType.PARROT, 105);
-    public static final Mob VILLAGER = new Mob(EntityType.VILLAGER, 120);
+    private static final List<IMob> MOBS = new ArrayList<>();
 
     private final ILandLord plugin;
 
     public MobsManager(ILandLord plugin) {
         this.plugin = plugin;
+
+        Mob ELDER_GUARDIAN = new Mob(EntityType.ELDER_GUARDIAN, Skulls.ELDER_GUARDIAN.getSkull(plugin));
+        Mob WITHER_SKELETON = new Mob(EntityType.WITHER_SKELETON, new ItemStack(Material.SKULL_ITEM, 1, (short) 1));
+        Mob STRAY = new Mob(EntityType.STRAY, Skulls.STRAY.getSkull(plugin));
+        Mob HUSK = new Mob(EntityType.HUSK, Skulls.HUSK.getSkull(plugin));
+        Mob ZOMBIE_VILLAGER = new Mob(EntityType.ZOMBIE_VILLAGER, Skulls.ZOMBIE_VILLAGER.getSkull(plugin));
+        Mob SKELETON_HORSE = new Mob(EntityType.SKELETON_HORSE, Skulls.SKELETON_HORSE.getSkull(plugin));
+        Mob ZOMBIE_HORSE = new Mob(EntityType.ZOMBIE_HORSE, Skulls.ZOMBIE_HORSE.getSkull(plugin));
+        Mob DONKEY = new Mob(EntityType.DONKEY, Skulls.DONKEY.getSkull(plugin));
+        Mob MULE = new Mob(EntityType.MULE, Skulls.MULE.getSkull(plugin));
+        Mob EVOKER = new Mob(EntityType.EVOKER, Skulls.EVOKER.getSkull(plugin));
+        Mob VEX = new Mob(EntityType.VEX, Skulls.VEX.getSkull(plugin));
+        Mob VINDICATOR = new Mob(EntityType.VINDICATOR, Skulls.VINDICATOR.getSkull(plugin));
+        Mob CREEPER = new Mob(EntityType.CREEPER, new ItemStack(Material.SKULL_ITEM, 1, (short) 4));
+        Mob SKELETON = new Mob(EntityType.SKELETON, new ItemStack(Material.SKULL_ITEM, 1, (short) 0));
+        Mob SPIDER = new Mob(EntityType.SPIDER, Skulls.SPIDER.getSkull(plugin));
+        Mob ZOMBIE = new Mob(EntityType.ZOMBIE, new ItemStack(Material.SKULL_ITEM, 1, (short) 2));
+        Mob SLIME = new Mob(EntityType.SLIME, Skulls.SLIME.getSkull(plugin));
+        Mob GHAST = new Mob(EntityType.GHAST, Skulls.GHAST.getSkull(plugin));
+        Mob PIG_ZOMBIE = new Mob(EntityType.PIG_ZOMBIE, Skulls.ZOMBIFIED_PIGLIN.getSkull(plugin));
+        Mob ENDERMAN = new Mob(EntityType.ENDERMAN, Skulls.ENDERMAN.getSkull(plugin));
+        Mob CAVE_SPIDER = new Mob(EntityType.CAVE_SPIDER, Skulls.CAVE_SPIDER.getSkull(plugin));
+        Mob SILVERFISH = new Mob(EntityType.SILVERFISH, Skulls.SILVERFISH.getSkull(plugin));
+        Mob BLAZE = new Mob(EntityType.BLAZE, Skulls.BLAZE.getSkull(plugin));
+        Mob MAGMA_CUBE = new Mob(EntityType.MAGMA_CUBE, Skulls.MAGMA_CUBE.getSkull(plugin));
+        Mob BAT = new Mob(EntityType.BAT, Skulls.BAT.getSkull(plugin));
+        Mob WITCH = new Mob(EntityType.WITCH, Skulls.WITCH.getSkull(plugin));
+        Mob GUARDIAN = new Mob(EntityType.GUARDIAN, Skulls.GUARDIAN.getSkull(plugin));
+        Mob PIG = new Mob(EntityType.PIG, Skulls.PIG.getSkull(plugin));
+        Mob SHEEP = new Mob(EntityType.SHEEP, Skulls.WHITE_SHEEP.getSkull(plugin));
+        Mob COW = new Mob(EntityType.COW, Skulls.COW.getSkull(plugin));
+        Mob CHICKEN = new Mob(EntityType.CHICKEN, Skulls.CHICKEN.getSkull(plugin));
+        Mob SQUID = new Mob(EntityType.SQUID, Skulls.SQUID.getSkull(plugin));
+        Mob WOLF = new Mob(EntityType.WOLF, Skulls.WOLF.getSkull(plugin));
+        Mob OCELOT = new Mob(EntityType.OCELOT, Skulls.OCELOT.getSkull(plugin));
+        Mob HORSE = new Mob(EntityType.HORSE, Skulls.WHITE_HORSE.getSkull(plugin));
+        Mob RABBIT = new Mob(EntityType.RABBIT, Skulls.BROWN_RABBIT.getSkull(plugin));
+        Mob POLAR_BEAR = new Mob(EntityType.POLAR_BEAR, Skulls.POLAR_BEAR.getSkull(plugin));
+        Mob LLAMA = new Mob(EntityType.LLAMA, Skulls.CREAMY_LLAMA.getSkull(plugin));
+        Mob PARROT = new Mob(EntityType.PARROT, Skulls.RED_PARROT.getSkull(plugin));
+        Mob VILLAGER = new Mob(EntityType.VILLAGER, Skulls.UNEMPLOYED_VILLAGER.getSkull(plugin));
+
         MOBS.sort(Comparator.comparing(iMob -> iMob.getType().name()));
     }
 
@@ -90,26 +93,26 @@ public class MobsManager implements IMobManager {
 
     public static class Mob implements IMob {
 
-        final EntityType t;
-        final int egg;
+        final EntityType type;
+        final ItemStack head;
         final String permission;
 
-        Mob(EntityType t, int egg) {
-            this.t = t;
-            this.egg = egg;
-            this.permission = "landlord.player.manage.mob-spawning." + t.name();
+        Mob(EntityType type, ItemStack head) {
+            this.type = type;
+            this.head = head;
+            this.permission = "landlord.player.manage.mob-spawning." + type.name();
 
             MOBS.add(this);
         }
 
         @Override
         public EntityType getType() {
-            return t;
+            return type;
         }
 
         @Override
-        public ItemStack getEgg() {
-            return new ItemStack(Material.MONSTER_EGG, 1, (short) egg);
+        public ItemStack getHead() {
+            return head.clone();
         }
 
         public String getPermission() {
@@ -118,7 +121,7 @@ public class MobsManager implements IMobManager {
 
         @Override
         public String getNiceName() {
-            String s = t.getName();
+            String s = type.getName();
             StringBuilder sb = new StringBuilder();
             sb.append(Character.toUpperCase(s.charAt(0)));
             for (int i = 1; i < s.length(); i++) {
@@ -135,7 +138,7 @@ public class MobsManager implements IMobManager {
 
         @Override
         public String getName() {
-            return t.getName().toUpperCase();
+            return type.getName().toUpperCase();
         }
     }
 }
