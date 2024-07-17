93655e55dd53867a7a3b7b9a649e37c840f97412
Aurélien <aurelien.domino@gmail.com>
2024-07-17 11:51:17 +0200

    fix: Fix GameProfile skull handling

--- LandLord-core/src/main/java/biz/princeps/landlord/util/Skulls.java
+++ LandLord-core/src/main/java/biz/princeps/landlord/util/Skulls.java
@@ -92,7 +92,7 @@ public enum Skulls {
             return head;
 
         SkullMeta headMeta = (SkullMeta) head.getItemMeta();
-        GameProfile profile = new GameProfile(uuid, uuid.toString());
+        GameProfile profile = new GameProfile(uuid, uuid.toString().substring(0, 16));
 
         profile.getProperties().put("textures", new Property("textures", texture));
 
